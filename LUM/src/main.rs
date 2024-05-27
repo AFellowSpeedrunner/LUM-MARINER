fe4f3aea409650128138b136a4a3fa203c19e974
MrMasterKeyboard <73440604+AFellowSpeedrunner@users.noreply.github.com>
2024-05-27 22:13:36 +0100

    main.rs updates to be compatible with new printing code.

--- LUM/src/main.rs
+++ LUM/src/main.rs
@@ -9,24 +9,22 @@ mod wait;
 // This function is called on panic.
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    print::clear_screen();
-
     // Extract the location if available
     if let Some(location) = info.location() {
-        print::log_formatted(format_args!(
+        println!("{}", format_args!(
             "PANIC: at file '{}' line {}\n",
             location.file(),
             location.line()
         ));
     } else {
-        print::log_formatted(format_args!("PANIC: Location unknown.\n"));
+        println!("{}", format_args!("PANIC: Location unknown.\n"));
     }
 
     // Extract the payload if available
     if let Some(payload) = info.payload().downcast_ref::<&str>() {
-        print::log_formatted(format_args!("Message: {}\n", payload));
+        println!("{}", format_args!("Message: {}\n", payload));
     } else {
-        print::log_formatted(format_args!("Message: Well, something happened.\n"));
+        println!("{}", format_args!("Message: Well, something happened.\n"));
     }
 
     loop {}
@@ -37,10 +35,11 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> () {
     let hello = "Hello, LUM/MARINER!\n";
     let oh = "Oh and...\n";
-    let newline = "We now have newline support in LUM!";
+    let newline = "We now have a better print system in LUM!\n";
+    let thanksandsorry = "Credits to Phillip Oppermann for the print code, sorry I stole it from blog_os... nothing was working and I was desperate...\n";
 
     // Log messages to the screen
-    print::log_formatted(format_args!("{}\n{}\n{}", hello, oh, newline));
+    println!("{}", format_args!("{}\n{}\n{}\n{}", hello, oh, newline, thanksandsorry));
 
     // Wait for 4 seconds
     wait::wait(4);
