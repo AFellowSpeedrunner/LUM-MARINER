f37134836ace70af9cc1f804e58f595cbe4d819a
MrMasterKeyboard <73440604+AFellowSpeedrunner@users.noreply.github.com>
2024-05-31 00:26:55 +0100

    Half of this doesn't work, but who gives a fu-

--- LUM/src/main.rs
+++ LUM/src/main.rs
@@ -5,34 +5,25 @@ extern crate alloc;
 
 use core::panic::PanicInfo;
 use LUM::println;
-use LUM::task::{executor::Executor, keyboard, Task};
 use bootloader::{entry_point, BootInfo};
-
-mod wait;
+use LUM::task::{executor::Executor, keyboard, Task};
+use alloc::string::String;
 
 // This function is called on panic.
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     // Extract the location if available
     if let Some(location) = info.location() {
-        println!("{}", format_args!(
-            "PANIC: at file '{}' line {}\n",
-            location.file(),
-            location.line()
-        ));
+        println!("PANIC: at file '{}' line {}", location.file(), location.line());
     } else {
-        println!("{}", format_args!("PANIC: Location unknown.\n"));
+        println!("PANIC: Location unknown.");
     }
 
-    // Extract the payload if available
-    if let Some(payload) = info.payload().downcast_ref::<&str>() {
-        println!("{}", format_args!("Message: {}\n", payload));
-    } else {
-        println!("{}", format_args!("Message: Well, something happened.\n"));
-    }
+    println!("Message: Well, something happened.");
 
     LUM::hlt_loop();
 }
+
 
 entry_point!(lum_main);
 
@@ -49,33 +40,29 @@ fn lum_main(boot_info: &'static BootInfo) -> ! {
     // Log messages to the screen
     println!("{}", format_args!("{}\n{}\n{}\n{}", hello, oh, newline, thanksandsorry));
 
-    // Prints dots, unneeded right now.
-    // LUM::init();
-
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
+    // Prints dots, unneeded right now.
+    // LUM::init();
+
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    // Keyboard press print code
+    // Keyboard press code
     // let mut executor = Executor::new();
-    // executor.spawn(Task::new(example_task()));
+    // executor.spawn(Task::new(load_shell()));
     // executor.spawn(Task::new(keyboard::print_keypresses()));
     // executor.run();
 
-    // Wait for 4 seconds
-    wait::wait(4);
-
-    // Trigger a panic to demonstrate panic handling
-    panic!("");
+    // Trigger a panic as we don't have anything else to do.
+    panic!("No other tasks, bailing out for safety...");
 }
 
 async fn async_number() -> u32 {
     42
 }
 
-async fn example_task() {
-    let number = async_number().await;
-    println!("async number: {}", number);
+async fn load_shell() {
+    // LUM::ulsh_main();
 }
