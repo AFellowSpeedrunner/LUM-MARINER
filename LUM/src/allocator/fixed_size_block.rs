cd1e19341f8aac47ee47097d1aaf725d5916c440
MrMasterKeyboard <73440604+AFellowSpeedrunner@users.noreply.github.com>
2024-05-30 20:51:44 +0100

    Ensure compliance with new version of linked_list_allocators to mitigate security vulnerability.

--- LUM/src/allocator/fixed_size_block.rs
+++ LUM/src/allocator/fixed_size_block.rs
@@ -43,7 +43,7 @@ impl FixedSizeBlockAllocator {
     /// This function is unsafe because the caller must guarantee that the given
     /// heap bounds are valid and that the heap is unused. This method must be
     /// called only once.
-    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+    pub unsafe fn init(&mut self, heap_start: *mut u8, heap_size: usize) {
         self.fallback_allocator.init(heap_start, heap_size);
     }
 
