53b8caf79cce517a6570b6c0a9d01d6ce63bf640
MrMasterKeyboard <73440604+AFellowSpeedrunner@users.noreply.github.com>
2024-06-11 00:30:23 +0100

    Fix the dots issue in interrupts.rs

--- LUM/src/interrupts.rs
+++ LUM/src/interrupts.rs
@@ -1,4 +1,4 @@
-use crate::{gdt, hlt_loop, print, println};
+use crate::{gdt, hlt_loop, println};
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 use spin;
@@ -72,7 +72,8 @@ extern "x86-interrupt" fn double_fault_handler(
 }
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    print!(".");
+    // CAUGHT CHA!
+    // print!(".");
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
