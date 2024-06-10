c3549c5506901d07d54f344a8c1b7683611b85d5
MrMasterKeyboard <73440604+AFellowSpeedrunner@users.noreply.github.com>
2024-06-11 00:33:54 +0100

    Somehow make the keyboard driver crash the system + minor changes + add the shell loader which barely works

--- LUM/src/main.rs
+++ LUM/src/main.rs
@@ -7,7 +7,6 @@ use core::panic::PanicInfo;
 use LUM::println;
 use bootloader::{entry_point, BootInfo};
 use LUM::task::{executor::Executor, keyboard, Task};
-use alloc::string::String;
 
 // This function is called on panic.
 #[panic_handler]
@@ -24,7 +23,6 @@ fn panic(info: &PanicInfo) -> ! {
     LUM::hlt_loop();
 }
 
-
 entry_point!(lum_main);
 
 fn lum_main(boot_info: &'static BootInfo) -> ! {
@@ -32,37 +30,32 @@ fn lum_main(boot_info: &'static BootInfo) -> ! {
     use LUM::memory::{self, BootInfoFrameAllocator};
     use x86_64::VirtAddr;
 
-    let hello = "Hello, LUM/MARINER!\n";
+    let ahoy = "Ahoy, LUM/MARINER!\n";
     let oh = "Oh and...\n";
-    let newline = "We now have a better print system in LUM!\n";
+    let shell = "We now have a non-functional shell in LUM...\n";
     let thanksandsorry = "Credits to Phillip Oppermann for literally the entire current base of this kernel, sorry I stole it from blog_os... I guess I'm too lazy... sorry once again...\n";
 
     // Log messages to the screen
-    println!("{}", format_args!("{}\n{}\n{}\n{}", hello, oh, newline, thanksandsorry));
+    println!("{}", format_args!("{}\n{}\n{}\n{}", ahoy, oh, shell, thanksandsorry));
+
+    // Figured it out.
+    LUM::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    // Prints dots, unneeded right now.
-    // LUM::init();
-
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    // Keyboard press code
-    // let mut executor = Executor::new();
-    // executor.spawn(Task::new(load_shell()));
-    // executor.spawn(Task::new(keyboard::print_keypresses()));
-    // executor.run();
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.spawn(Task::new(load_shell()));
+    executor.run();
 
     // Trigger a panic as we don't have anything else to do.
     panic!("No other tasks, bailing out for safety...");
 }
 
-async fn async_number() -> u32 {
-    42
-}
-
 async fn load_shell() {
-    // LUM::ulsh_main();
+    LUM::ulsh::shell::ulsh_main().await;
 }
